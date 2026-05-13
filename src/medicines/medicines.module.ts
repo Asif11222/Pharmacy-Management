ab6c2c4accc22245226499d91d887dc3928fb514
Asif11222 <shariarasif420@gmail.com>
2026-05-13 15:05:40 +0600

    connect medicine entity,service and controller togather

--- src/medicines/medicines.module.ts
+++ src/medicines/medicines.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MedicinesController } from './medicines.controller';
 import { MedicinesService } from './medicines.service';
+import { Medicine } from './medicine.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Medicine])],
   controllers: [MedicinesController],
   providers: [MedicinesService]
 })
