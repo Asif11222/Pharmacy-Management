cad960a22acad8ec5fd0484603a95584e3b586c7
Asif11222 <shariarasif420@gmail.com>
2026-05-13 15:31:47 +0600

    medicine api endpoints

--- src/medicines/medicines.controller.ts
+++ src/medicines/medicines.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  } from '@nestjs/common';
+
+  import { MedicinesService } from './medicines.service';
+import { MedicineDto } from './dto/medicine.dto';
+import { UpdateMedicineDto } from './dto/update-medicine.dto';
 
 @Controller('medicines')
-export class MedicinesController {}
+export class MedicinesController {
+
+    constructor(private readonly medicinesService: MedicinesService) {}
+
+  @Post()
+  create(@Body() medicineDto: MedicineDto) {
+    return this.medicinesService.create(medicineDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.medicinesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.medicinesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateMedicineDto: UpdateMedicineDto,
+  ) {
+    return this.medicinesService.update(id, updateMedicineDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.medicinesService.remove(id);
+  }
+}
