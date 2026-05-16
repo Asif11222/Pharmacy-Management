cd0448e4900a542bf87a31ed5c6687de63255acb
Asif11222 <shariarasif420@gmail.com>
2026-05-16 16:56:20 +0600

    Connects order realated files togather

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { Order } from './order.entity';
+import { OrderItem } from './order-item.entity';
+import { Medicine } from '../medicines/medicine.entity';
 
 @Module({
+   imports: [TypeOrmModule.forFeature([Order, OrderItem, Medicine])],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
