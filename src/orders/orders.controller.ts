b75d0fd4ea5e97e1ede318027826cf8bf4af1257
Asif11222 <shariarasif420@gmail.com>
2026-05-16 17:16:29 +0600

    creates the API endpoints for the Orders Module

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,45 @@
-import { Controller } from '@nestjs/common';
+import { Body,Controller,Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post, } from '@nestjs/common';
+
+  import { OrdersService } from './orders.service';
+import { OrderDto } from './dto/order.dto';
+import { OrderStatus } from './order.entity';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+
+     constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  create(@Body() orderDto: OrderDto) {
+    return this.ordersService.create(orderDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.ordersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.findOne(id);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status') status: OrderStatus,
+  ) {
+    return this.ordersService.updateStatus(id, status);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.ordersService.remove(id);
+  }
+
+}
