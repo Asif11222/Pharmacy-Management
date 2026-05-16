6abe3418ebb06bd9be9a94458ac7277aebd5e699
Asif11222 <shariarasif420@gmail.com>
2026-05-16 16:46:22 +0600

    validates order request when user place order thay send medicine id and quantity

---
+++ src/orders/order.dto.ts
@@ -0,0 +1,31 @@
+// src/orders/dto/order.dto.ts
+
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class OrderItemDto {
+  @IsNotEmpty()
+  @IsNumber()
+  medicineId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+}
+
+export class OrderDto {
+  @IsOptional()
+  @IsNumber()
+  userId?: number;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => OrderItemDto)
+  items: OrderItemDto[];
+}
