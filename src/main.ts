fae4680fa37b77b80c6497008b6ab33c7c682db3
Asif11222 <shariarasif420@gmail.com>
2026-05-18 03:02:58 +0600

    Installed swagger Packages

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Pharmacy Management API')
+    .setDescription('Backend API documentation for Pharmacy Management System')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
