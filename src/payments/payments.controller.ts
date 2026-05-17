560bd360c5b0d2942bfbee9cb8aecb27739012ff
Asif11222 <shariarasif420@gmail.com>
2026-05-18 02:49:25 +0600

    Payment Api endpoints

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {  Body, 
+    Controller,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    ParseIntPipe,
+    Patch,
+    Post, 
+
+} from '@nestjs/common';
 
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+
+    constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post()
+  create(@Body() paymentDto: PaymentDto) {
+    return this.paymentsService.create(paymentDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.paymentsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.paymentsService.findOne(id);
+  }
+
+  @Patch(':id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status') status: PaymentStatus,
+  ) {
+    return this.paymentsService.updateStatus(id, status);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.paymentsService.remove(id);
+  }
+
+}
