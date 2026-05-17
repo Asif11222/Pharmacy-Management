355269df3fc4184c486dc335666c2889d0f9e245
Asif11222 <shariarasif420@gmail.com>
2026-05-18 02:43:02 +0600

    connect payment module files togather

--- src/payments/payments.module.ts
+++ src/payments/payments.module.ts
@@ -2,7 +2,14 @@ import { Module } from '@nestjs/common';
 import { PaymentsController } from './payments.controller';
 import { PaymentsService } from './payments.service';
 
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Payment } from './payment.entity';
+import { Order } from '../orders/order.entity';
+
+
 @Module({
+  imports: [TypeOrmModule.forFeature([Payment, Order])],
   controllers: [PaymentsController],
   providers: [PaymentsService]
 })
