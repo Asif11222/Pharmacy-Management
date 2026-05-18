72a501a3b914c5a96193b04f07e95971a451ec3c
Asif11222 <shariarasif420@gmail.com>
2026-05-18 07:09:35 +0600

    Install axios package and connected with fronted

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    credentials: true,
+  });
+
+
+
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
