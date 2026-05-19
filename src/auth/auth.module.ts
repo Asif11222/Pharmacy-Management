62f0543d9df251b174e1311dfb8cba2e33b091db
Asif11222 <shariarasif420@gmail.com>
2026-05-20 00:17:16 +0600

    groups everything related to auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,20 +2,25 @@
 
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from '../users/users.module';
+import { MailModule } from '../mail/mail.module';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UsersModule,
+    MailModule,
+    PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'mysecret',
       signOptions: { expiresIn: '7d' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
