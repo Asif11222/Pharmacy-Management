0c016d925c588b4504905d61d40353799ef7fa33
Asif11222 <shariarasif420@gmail.com>
2026-05-20 01:09:06 +0600

    connects everithing in order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -6,9 +6,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './order.entity';
 import { OrderItem } from './order-item.entity';
 import { Medicine } from '../medicines/medicine.entity';
+import { Payment } from '../payments/payment.entity';
 
 @Module({
-   imports: [TypeOrmModule.forFeature([Order, OrderItem, Medicine])],
+  imports: [TypeOrmModule.forFeature([Order, OrderItem, Medicine, Payment])],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
